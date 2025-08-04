e1d7099b41785e55dd70f9bae28b92325137e58c
krevetkqqq <antosinoftenksvov@gmail.com>
2025-08-05 01:38:28 +0300

    .

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import asyncio
+from typing import Optional
 import aiogram
-from aiogram.types import FSInputFile, InputFileUnion, InputMediaVideo
+from aiogram.types import FSInputFile
 from aiogram.filters import Command
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 import os
 from aiogram import types
-from yandex_music import Client
+from yandex_music import Client, Search
 import yt_dlp
 import time
 from pathlib import Path
@@ -17,6 +20,8 @@ client = Client(token_music).init()
 bot = aiogram.Bot(token)
 dp = aiogram.Dispatcher()
 router = aiogram.Router()
+
+global_search_result: Optional[Search] = None
 
 def timestamp():
     return int(time.time() * 1000)
@@ -51,6 +56,7 @@ async def music_cmd(message = types.Message):
 
 @dp.message()
 async def notcmd(message = types.Message):
+    global global_search_result
     if str(message.text).upper().startswith("ПЕСНЯ") or str(message.text).upper().startswith("НАЙТИ"):     
         textmess = message.text[5:]
         search_result = client.search(textmess)
@@ -70,8 +76,12 @@ async def notcmd(message = types.Message):
                 first = client.search(music_title)['best']['result']
                 name = music_title + ', ' + search_result.best.result.name
             elif(wereAlbum):
-                first = search_result["tracks"]["results"][0]
-                name = search_result["tracks"]["results"][0]["title"] + ',' + search_result["tracks"]["results"][0]["artists"][0]["name"]
+                # first = search_result["tracks"]["results"][0]
+                # name = search_result["tracks"]["results"][0]["title"] + ',' + search_result["tracks"]["results"][0]["artists"][0]["name"]
+                for index, track in enumerate(search_result["tracks"]["results"]):
+                    await message.reply(str(index) + ", "+ track["artists"][0]["name"] + ", " + track["title"])
+                global_search_result = search_result
+                return
             else:
                 first = search_result['best']['result']
                 name = search_result.best.result.title + ', ' + search_result.best.result.artists[0]['name']
@@ -101,8 +111,21 @@ async def notcmd(message = types.Message):
             await message.reply("что-то пошло не так")
         finally:
             os.remove(name)
+    else:
+        if True:
+            id = int(message.text)
+            first = global_search_result["tracks"]["results"][id]
+            name = global_search_result["tracks"]["results"][id]["title"] + ', ' + global_search_result["tracks"]["results"][id]["artists"][0]["name"]
+            name = name + ".mp3"
+            file = FSInputFile(name)
+            first.download(name)
+            await message.reply_audio(file)
+            os.remove(name)
+        # except:
+        #     await message.reply("что-то пошло не так")
 
 
+    
 async def main():
     dp.include_router(router)
 
